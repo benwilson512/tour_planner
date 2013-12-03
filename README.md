d68058cb18e3ae4d03d55b9efa659c8d6e8ee204
Ben Wilson <benwilson512@gmail.com>
2013-12-03 13:14:12 -0500

    create some db setup tasks and update readme

--- README.md
+++ README.md
@@ -12,13 +12,23 @@
 
   It may ask you something, say yes.
 
-4. Run server:
+4. Setup database:
+      
+        mix do compile, db.setup
+
+  If this fails it's most likely because the port to your postgres service isn't correct. Contact me in this case.
+
+5. Run server:
 
         iex -S mix server
 
-5. Browse to:
+6. Load seed data. Enter the following into the REPL that the previous command created. You should see a fair bit of output.
 
-        http://localhost:4000
+        Repo.Seeds.run
+
+7. Browse to:
+
+        http://localhost:4000/routes
 
 Assets are in
 
--- lib/tasks/db.ex
+++ lib/tasks/db.ex
@@ -1,10 +1,27 @@
 defmodule Mix.Tasks.Db do
 
+  defmodule Nuke do
+    use Mix.Task
+    def run(_) do
+      Mix.shell.cmd("dropdb -U #{Repo.config["user"]} -e #{Repo.config["database"]}")
+      Mix.shell.cmd("dropuser #{Repo.config["user"]}")
+    end
+  end
+
+  defmodule Setup do
+    use Mix.Task
+    def run(_) do
+      Mix.shell.cmd("createuser #{Repo.config["user"]} -d")
+      Mix.shell.cmd("createdb -U #{Repo.config["user"]} -e #{Repo.config["database"]}")
+      Mix.Task.run "ecto.migrate", [Repo]
+    end
+  end
+
   defmodule Reset do
     use Mix.Task
     def run(_) do
-      Mix.shell.cmd("dropdb -e #{Repo.config["database"]}")
-      Mix.shell.cmd("createdb -e #{Repo.config["database"]}")
+      Mix.shell.cmd("dropdb -U #{Repo.config["user"]} -e #{Repo.config["database"]}")
+      Mix.shell.cmd("createdb -U #{Repo.config["user"]} -e #{Repo.config["database"]}")
       Mix.Task.run "ecto.migrate", [Repo]
     end
   end
@@ -13,6 +30,7 @@ defmodule Mix.Tasks.Db do
     use Mix.Task
     def run(_) do
       Mix.Task.run "db.reset"
+      TourPlanner.start
       Repo.Seeds.run
     end
   end
