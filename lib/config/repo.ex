617aa0064b3cb032c58b73e99291a37be75dc2d7
Ben Wilson <benwilson512@gmail.com>
2013-12-02 20:48:23 -0500

    only use port locally

--- lib/config/repo.ex
+++ lib/config/repo.ex
@@ -9,7 +9,7 @@ defmodule Repo do
 
   def url do
     c = config
-    "ecto://#{c["user"]}:#{c["password"]}@localhost:15432/#{c["database"]}"
+    "ecto://#{c["user"]}:#{c["password"]}@localhost:#{c["port"]}/#{c["database"]}"
   end
 
   def priv do
